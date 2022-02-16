152d240b4c796e3ea50919d5278364d7d822c4b0
Hasan KAHRAMAN <hasankahraman06092@gmail.com>
2022-02-16 15:02:12 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir cümle yazınız:");
+            string girdi = Console.ReadLine();
+            ArrayList sesliDizi = new ArrayList{'a','e','ı','i','o','ö','u','ü'};
+            ArrayList girdininSeslileri = new ArrayList();
+
+            foreach(char c in girdi){
+                foreach(char sesli in sesliDizi){
+                    if(c == sesli){
+                        girdininSeslileri.Add(c);
+                    }
+                }
+            }
+
+            girdininSeslileri.Sort();
+            foreach(var sesli in girdininSeslileri){
+                Console.WriteLine(sesli);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
